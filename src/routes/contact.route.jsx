a04b6ed23f5cab2d45f5d032ade2f24cef4dfe05
Della Duck <aurelien.strimelle@bstorm.be>
2025-08-12 15:26:21 +0200

    Jotai 👻

--- src/routes/contact.route.jsx
+++ src/routes/contact.route.jsx
@@ -1,8 +1,20 @@
+import { useAtom, useSetAtom } from "jotai";
+import { countAtom } from "../atoms/count.atom";
+
 export default function ContactPage() {
+
+    // const [count, setCount] = useAtom(countAtom);
+    const setCount = useSetAtom(countAtom);
+    
+    
+    const handleIncr = () => {
+        setCount(prevCount => prevCount + 1)
+    }
 
     return (
         <>
             <h1>Contactez moi</h1>
+            <button onClick={handleIncr}>+1</button>
         </>
     );
 }
--- src/routes/home.route.jsx
+++ src/routes/home.route.jsx
@@ -1,11 +1,18 @@
+import { useAtom, useAtomValue } from "jotai";
+import { countAtom } from "../atoms/count.atom";
+
 export default function HomePage() {
 
     const urlImg = '/images/duck.jpg';
     // Pour les images, attention à la casse (vérification React-router)
 
+    // const [count, setCount] = useAtom(countAtom);
+    const count = useAtomValue(countAtom);
+
     return (
         <>
             <h1>Accueil</h1>
+            <p>Compteur : {count}</p>
             {/* <img alt="Della Duck" src="/images/duck.jpg" style={{ width: '100%' }} /> */}
             <img alt="Della Duck" src={urlImg} style={{ width: '100%' }} />
         </>
