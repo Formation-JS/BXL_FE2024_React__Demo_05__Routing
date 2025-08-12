c783b7fb9eac731f885666a1182a625c607d17d2
Della Duck <aurelien.strimelle@bstorm.be>
2025-08-12 10:37:07 +0200

    Image from "public" folder 👀

--- src/routes/home.route.jsx
+++ src/routes/home.route.jsx
@@ -1,8 +1,13 @@
 export default function HomePage() {
 
+    const urlImg = '/images/duck.jpg';
+    // Pour les images, attention à la casse (vérification React-router)
+
     return (
         <>
             <h1>Accueil</h1>
+            {/* <img alt="Della Duck" src="/images/duck.jpg" style={{ width: '100%' }} /> */}
+            <img alt="Della Duck" src={urlImg} style={{ width: '100%' }} />
         </>
     );
 }
