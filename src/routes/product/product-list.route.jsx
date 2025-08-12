e5da04c0b15ec37119de2f4fb34ae8d0dc1d6506
Della Duck <aurelien.strimelle@bstorm.be>
2025-08-12 11:13:20 +0200

    SearchParams 🐮

--- src/routes/product/product-list.route.jsx
+++ src/routes/product/product-list.route.jsx
@@ -1,13 +1,25 @@
-import { Link } from 'react-router';
+import { Link, useSearchParams } from 'react-router';
 import products from './../../data/product.json';
+import { useState } from 'react';
 
 export default function ProductListPage() {
+    
+    const [searchParams, setSearchParams] = useSearchParams()
+    // console.log('Param de l\'url : ', searchParams.get('example'))
+
+    const query = searchParams.get('query') ?? '';
+
+
+    // const [query, setQuery] = useState('');
+    const productFilter = products.filter(p => p.nom.includes(query));
 
     return (
         <>
             <h1>La liste des produits</h1>
+            {/* <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} /> */}
+            <input type="text" value={query} onChange={(e) => setSearchParams({query: e.target.value})} />
             <ul>
-                {products.map(product => (
+                {productFilter.map(product => (
                     <li>
                         <Link to={product.id.toString()}>{product.nom}</Link>
                     </li>
